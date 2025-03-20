dc817977ea3bff5609af95963bfe332bcd4c4069
Mika Senghaas <mail@mikasenghaas.de>
2025-03-20 19:38:14 +0000

    Added runtime into the sender/ receiver class

--- src/receiver.rs
+++ src/receiver.rs
@@ -2,84 +2,89 @@ use anyhow::{Context, Result};
 use iroh::{Endpoint, endpoint::{Connection, RecvStream}};
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use tokio::runtime::Runtime;
 
 const ALPN: &[u8] = b"hello-world";
 
-#[derive(Clone)]
 pub struct IrohReceiver {
+    runtime: Runtime,
     endpoint: Endpoint,
     connection: Connection,
     recv_streams: Vec<Arc<Mutex<RecvStream>>>,
 }
 
 impl IrohReceiver {
-    pub async fn new(num_streams: usize) -> Result<Self> {
-        println!("Receiver starting...");
-        let endpoint = Endpoint::builder()
-            .discovery_n0()
-            .alpns(vec![ALPN.to_vec()])
-            .bind()
-            .await?;
-        println!("cargo run --bin sender {}", endpoint.node_id());
-        let connection = endpoint.accept().await.context("no incoming connection")?.await?;
-        let mut recv_streams = Vec::with_capacity(num_streams);
-        for _ in 0..num_streams {
-            let recv_stream = connection.accept_uni().await?;
-            recv_streams.push(Arc::new(Mutex::new(recv_stream)));
-        }
+    pub fn new(num_streams: usize) -> Result<Self> {
+        let runtime = Runtime::new()?;
+        let (endpoint, connection, recv_streams) = runtime.block_on(async {
+            let endpoint = Endpoint::builder()
+                .discovery_n0()
+                .alpns(vec![ALPN.to_vec()])
+                .bind()
+                .await?;
+            println!("cargo run --bin sender {}", endpoint.node_id());
+            let connection = endpoint.accept().await.context("no incoming connection")?.await?;
+            let mut recv_streams = Vec::with_capacity(num_streams);
+            for _ in 0..num_streams {
+                let recv_stream = connection.accept_uni().await?;
+                recv_streams.push(Arc::new(Mutex::new(recv_stream)));
+            }
+            Ok::<(Endpoint, Connection, Vec<Arc<Mutex<RecvStream>>>), anyhow::Error>((endpoint, connection, recv_streams))
+        })?;
 
-        Ok(Self { endpoint, connection, recv_streams })
+        Ok(Self { runtime, endpoint, connection, recv_streams })
     }
 
-    pub async fn recv(&mut self, tag: usize) -> Result<Vec<u8>> {
-        // Lock the receiving stream
-        let stream = &mut self.recv_streams[tag];
-        let mut stream = stream.lock().await;
-
-        // Read the size of the message
-        let mut size = [0; 4];
-        stream.read_exact(&mut size).await?;
-        let size = u32::from_le_bytes(size) as usize;
-
-        // Read the message
-        let mut msg = vec![0; size];
-        stream.read_exact(&mut msg).await?;
-
-        Ok(msg)
-    }
-
-    pub async fn close(&mut self) -> Result<()> {
-        println!("Receiver closing...");
-        for stream in self.recv_streams.iter_mut() {
+    pub fn recv(&mut self, tag: usize) -> Result<Vec<u8>> {
+        self.runtime.block_on(async {
+            // Lock the receiving stream
+            let stream = &mut self.recv_streams[tag];
             let mut stream = stream.lock().await;
-            stream.stop(0u32.into())?;
-        }
-        self.connection.closed().await;
-        self.endpoint.close().await;
-        Ok(())
+
+            // Read the size of the message
+            let mut size = [0; 4];
+            stream.read_exact(&mut size).await?;
+            let size = u32::from_le_bytes(size) as usize;
+
+            // Read the message
+            let mut msg = vec![0; size];
+            stream.read_exact(&mut msg).await?;
+
+            Ok(msg)
+        })
+    }
+
+    pub fn close(&mut self) -> Result<()> {
+        self.runtime.block_on(async {
+            for stream in self.recv_streams.iter_mut() {
+                let mut stream = stream.lock().await;
+                stream.stop(0u32.into())?;
+            }
+            self.connection.closed().await;
+            self.endpoint.close().await;
+            Ok(())
+        })
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     // Setup
     let num_new_tokens = 10;
     let num_micro_batches = 4;
 
     // Create receiver
-    let mut receiver = IrohReceiver::new(num_micro_batches).await?;
+    let mut receiver = IrohReceiver::new(num_micro_batches)?;
 
     // Receive messages
     for _ in 0..num_new_tokens {
         for micro_batch_idx in 0..num_micro_batches {
-            let mut receiver_clone = receiver.clone();
-            let msg = receiver_clone.recv(micro_batch_idx).await;
-            println!("Received msg: {:?}", String::from_utf8(msg.unwrap()).unwrap());
+            let msg = receiver.recv(micro_batch_idx)?;
+            println!("Received msg: {:?}", String::from_utf8(msg).unwrap());
         }
     }
 
     // Close the receiver
-    receiver.close().await?;
+    receiver.close()?;
 
     Ok(())
 }
--- src/sender.rs
+++ src/sender.rs
@@ -1,50 +1,61 @@
 use anyhow::Result;
 use iroh::{NodeId, NodeAddr, Endpoint, endpoint::{Connection, SendStream}};
 use std::sync::Arc;
-use std::time::Duration;
 use tokio::sync::Mutex;
+use tokio::task::JoinHandle;
+use tokio::runtime::Runtime;
 
 const ALPN: &[u8] = b"hello-world";
 
-#[derive(Clone)]
 pub struct IrohSender {
+    runtime: Runtime,
     endpoint: Endpoint,
     connection: Connection,
     send_streams: Vec<Arc<Mutex<SendStream>>>,
 }
 
 impl IrohSender {
-    pub async fn new(addr: NodeAddr, num_streams: usize) -> Result<Self> {
-        println!("Sender starting...");
-        let endpoint = Endpoint::builder().discovery_n0().bind().await?;
-        let connection = endpoint.connect(addr, ALPN).await?;
-        let mut send_streams = Vec::with_capacity(num_streams);
-        for _ in 0..num_streams {
-            let send_stream = Arc::new(Mutex::new(connection.open_uni().await?));
-            send_streams.push(send_stream);
-        }
-
-        Ok(Self { endpoint, connection, send_streams })
+    pub fn new(addr: NodeAddr, num_streams: usize) -> Result<Self> {
+        let runtime = Runtime::new()?;
+        let (endpoint, connection, send_streams) = runtime.block_on(async {
+            let endpoint = Endpoint::builder().discovery_n0().bind().await?;
+            let connection = endpoint.connect(addr, ALPN).await?;
+            let mut send_streams = Vec::with_capacity(num_streams);
+            for _ in 0..num_streams {
+                let send_stream = Arc::new(Mutex::new(connection.open_uni().await?));
+                send_streams.push(send_stream);
+            }
+            Ok::<(Endpoint, Connection, Vec<Arc<Mutex<SendStream>>>), anyhow::Error>((endpoint, connection, send_streams))
+        })?;
+        Ok(Self { runtime, endpoint, connection, send_streams })
     }
 
-    pub async fn send(&mut self, msg: Vec<u8>, tag: usize) -> Result<()> {
-        let stream = &mut self.send_streams[tag];
-        let mut stream = stream.lock().await;
-        let size = msg.len() as u32;
-        stream.write_all(&size.to_le_bytes()).await?;
-        stream.write_all(&msg).await?;
-        Ok(())
-    }
-
-    pub async fn close(&mut self) -> Result<()> {
-        println!("Sender closing...");
-        for stream in self.send_streams.iter_mut() {
+    pub fn isend(&mut self, msg: Vec<u8>, tag: usize) -> JoinHandle<Result<()>> {
+        let stream = self.send_streams[tag].clone();
+        self.runtime.spawn(async move {
             let mut stream = stream.lock().await;
-            stream.stopped().await?;
-        }
-        self.connection.close(0u32.into(), b"close");
-        self.endpoint.close().await;
-        Ok(())
+            let size = msg.len() as u32;
+            stream.write_all(&size.to_le_bytes()).await?;
+            stream.write_all(&msg).await?;
+            Ok(())
+        })
+    }
+
+    pub fn send(&mut self, msg: Vec<u8>, tag: usize) -> Result<()> {
+        let handle = self.isend(msg, tag);  
+        self.runtime.block_on(handle)?
+    }
+
+    pub fn close(&mut self) -> Result<()> {
+        self.runtime.block_on(async {
+            for stream in self.send_streams.iter_mut() {
+                let mut stream = stream.lock().await;
+                stream.stopped().await?;
+            }
+            self.connection.close(0u32.into(), b"close");
+            self.endpoint.close().await;
+            Ok(())
+        })
     }
 } 
 
@@ -58,38 +69,26 @@ fn get_node_addr() -> Result<NodeAddr> {
     Ok(NodeAddr::new(node_id))
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     // Setup
     let num_new_tokens = 10;
     let num_micro_batches = 4;
 
     // Create sender
     let addr = get_node_addr()?;
-    let mut sender = IrohSender::new(addr, num_micro_batches).await?;
+    let mut sender = IrohSender::new(addr, num_micro_batches)?;
 
     // Send messages
-    let mut handles = vec![];
     for token_idx in 0..num_new_tokens {
         for micro_batch_idx in 0..num_micro_batches {
-            let mut sender_clone = sender.clone();  
-            let handle = tokio::spawn(async move {
-                let msg = format!("token_idx: {}, micro_batch_idx: {}", token_idx, micro_batch_idx);
-                println!("Sending msg: {}", msg);
-                // Pretend to do some work
-                tokio::time::sleep(Duration::from_secs(1)).await;
-                let _ = sender_clone.send(msg.as_bytes().to_vec(), micro_batch_idx).await;
-            });
-            handles.push(handle);
+            let msg = format!("token_idx: {}, micro_batch_idx: {}", token_idx, micro_batch_idx);
+            println!("Sending msg: {}", msg);
+            let _ = sender.send(msg.as_bytes().to_vec(), micro_batch_idx);
         }
-        for handle in handles.iter_mut() {
-            handle.await?;
-        }
-        handles.clear();
     }
 
     // Close the sender
-    sender.close().await?;
+    let _ = sender.close();
 
     Ok(())
 }
