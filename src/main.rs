61663af32aafecde93e7fe649dfca8b820899edb
Mika Senghaas <mail@mikasenghaas.de>
2025-03-21 02:02:41 +0000

    Added intended logic for node class

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ fn run_sender() -> Result<()> {
     // Create sender
     let mut node = Node::new(num_micro_batches)?;
     node.connect(get_node_addr()?)?;
+    while !node.can_send() {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
     println!("Sender is ready");
 
     // Send messages
@@ -82,6 +85,47 @@ fn run_receiver() -> Result<()> {
     Ok(())
 }
 
+fn input(prompt: &str) -> String {
+    use std::io::{self, Write};
+
+    // Print the prompt
+    print!("{}", prompt);
+    // Ensure the prompt is displayed before reading input
+    io::stdout().flush().expect("Failed to flush stdout");
+    
+    // Read user input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    // Trim the trailing newline and return
+    input.trim().to_string()
+}
+
+fn run_node(rank: &str) -> Result<()> {
+    let num_micro_batches = 2;
+    let mut node = Node::new(num_micro_batches)?;
+    println!("Connect to {}", node.node_id);
+    let peer_id = input("Please enter the peer ID: ");
+    let bytes = hex::decode(peer_id)?;
+    let peer_id = NodeId::from_bytes(bytes.as_slice().try_into()?)?;
+    let peer_addr = NodeAddr::new(peer_id);
+    node.connect(peer_addr)?;
+    while !node.is_ready() {
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+    println!("Connected");
+    if rank == "0" {
+        let msg = vec![0; 10];
+        node.isend(msg, 0).wait()?;
+    } else {
+        let msg = node.irecv(0).wait()?;
+        println!("Received message: {:?}", msg);
+    }
+
+    node.close()?;
+    Ok(())
+}
+
 fn main() {
     use std::env;
     let args: Vec<String> = env::args().collect();
@@ -91,6 +135,13 @@ fn main() {
     });
 
     match mode.as_str() {
+        "node" => {
+            let rank = args.get(2).unwrap_or_else(|| {
+                eprintln!("cargo run node <rank>");
+                std::process::exit(1);
+            });
+            run_node(rank).unwrap()
+        }
         "sender" => run_sender().unwrap(),
         "receiver" => run_receiver().unwrap(),
         _ => {
